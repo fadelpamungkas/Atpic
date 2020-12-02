f2cadd8cdcb3a1d94a4ee9c850edde550ee56fd4
Del <fadelpamungkas3@gmail.com>
2020-12-02 18:52:04 +0700

    bottom navigation bar + 4 fragments

---
+++ app/src/main/java/com/example/atpic/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.atpic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navigationController = Navigation.findNavController(this, R.id.navigationHost)
+
+        /**
+         * Call looks like NavigationUI.setupWithNavController(bottomNavigation, navigationController)
+         * for native BottomNavigationView
+         */
+        ExpandableBottomBarNavigationUI.setupWithNavController(expandable_bottom_bar, navigationController)
+
+    }
+}

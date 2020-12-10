5397597da063498c23581e39c57fb300865e9788
billyindrai@gmail.com <billyindrai@gmail.com>
2020-12-11 00:16:20 +0700

    edit profile tapi belum jadi

--- app/src/main/java/com/android/atpic/EditProfileActivity.java
+++ app/src/main/java/com/android/atpic/EditProfileActivity.java
@@ -1,16 +1,125 @@
 package com.android.atpic;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EditProfileActivity extends AppCompatActivity {
+import com.android.atpic.model.Users;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class EditProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtEmail, edtPass;
+    private TextView tvName;
+    private Button btnEdit;
+
+    public static  final String EXTRA_USERS = "extra_users";
+    public final int ALERT_DIALOG_CLOSE = 10;
+    public final int ALERT_DIALOG_DELETE = 10;
+
+    private Users users;
+    private String usersId;
+
+    DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser authUsers = mAuth.getCurrentUser();
+
+        tvName = findViewById(R.id.name);
+        edtEmail = findViewById(R.id.edt_email);
+        edtPass = findViewById(R.id.edt_pass);
+        btnEdit = findViewById(R.id.btn_editprofile);
+        btnEdit.setOnClickListener(this);
+
+        users = getIntent().getParcelableExtra(EXTRA_USERS);
+
+        database = FirebaseDatabase.getInstance().getReference("users").child(authUsers.getUid());
+        database.addValueEventListener((new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users users = snapshot.getValue(Users.class);
+                tvName.setText(users.getName());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        }));
+
+        if(users != null){
+            usersId = users.getId();
+        }else{
+            users = new Users();
+        }
+
+        if(users != null){
+            edtEmail.setText(users.getEmail());
+            edtPass.setText(users.getPassword());
+        }
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Edit Data");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_editprofile){
+            updateUsers();
+        }
+    }
+
+    private void updateUsers() {
+        String email = edtEmail.getText().toString().trim();
+        String pass = edtPass.getText().toString().trim();
+
+        boolean isEmptyFields = false;
+
+        if(TextUtils.isEmpty(email)){
+            isEmptyFields = true;
+            edtEmail.setError("Field ini tidak boleh kosong");
+        }
+
+        if(TextUtils.isEmpty(pass)){
+            isEmptyFields = true;
+            edtPass.setError("Field ini tidak boleh kosong");
+        }
+
+        if(! isEmptyFields){
+            Toast.makeText(EditProfileActivity.this, "Edit Profile...", Toast.LENGTH_SHORT).show();
+
+            users.setEmail(email);
+            users.setPassword(pass);
+
+            DatabaseReference dbUsers = database.child("users");
+
+            dbUsers.child(usersId).setValue(users);
+
+            finish();
+        }
+
+    }
+
 
 }
